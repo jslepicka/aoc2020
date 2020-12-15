d4d9ce3ef8866100631da63de5f722f125ec86ab
James Slepicka <james@slepicka.net>
2020-12-15 00:57:45 -0600

    day 15 optimization

--- 15.py
+++ 15.py
@@ -6,16 +6,18 @@ with open("15.txt") as f:
 def part1(limit):
     hist = {}
     last = None
-    i = 1
+    
+    for i, val in enumerate(input):
+        val = int(val)
+        hist[val] = i + 1
+        speak = val
+    
+    i = len(input) + 1
     while i < limit + 1:
-        if i < len(input) + 1:
-            a = int(input[i-1])
-            speak = a
+        if last not in hist:
+            speak = 0
         else:
-            if last not in hist:
-                speak = 0
-            else:
-                speak = i - hist[last] - 1
+            speak = i - hist[last] - 1
         if last is not None:
             hist[last] = i-1
         last = speak
@@ -23,4 +25,9 @@ def part1(limit):
     return speak
 
 print("Part 1: %d" % part1(2020))
+
+start = time.time()
 print("Part 1: %d" % part1(30000000))
+print(time.time() - start)
+
+
