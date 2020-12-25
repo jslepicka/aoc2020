04a5c729d5e2e95435da441b67da4b1995b0136a
James Slepicka <james@slepicka.net>
2020-12-25 12:45:43 -0600

    day 25 optimization

--- 25.py
+++ 25.py
@@ -14,8 +14,19 @@ def part1():
         if pow(7, n, mod) == public_keys[1]:            
             break
         n += 1
-    
+    #now use the loop val with the other public key to get the encryption key
+    return pow(public_keys[0], n, mod)
+
+def part1a():
+    #forego the modular exponentiation stuff
+    mod = 20201227
+    n = 0
+    x = 1
+    while x != public_keys[1]:
+        x = (x * 7) % mod
+        n += 1
     #now use the loop val with the other public key to get the encryption key
     return pow(public_keys[0], n, mod)
 
 print("Part 1: %d" % part1())
+print("Part 1: %d" % part1a())
