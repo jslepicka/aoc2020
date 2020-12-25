6d26e4cd2aba5df665be50539d059ebb6a3bc9ed
James Slepicka <james@slepicka.net>
2020-12-25 13:00:07 -0600

    day 25 optimizations

--- 25.py
+++ 25.py
@@ -1,3 +1,4 @@
+from math import ceil, sqrt
 public_keys = []
 
 with open("25.txt") as f:
@@ -28,5 +29,27 @@ def part1a():
     #now use the loop val with the other public key to get the encryption key
     return pow(public_keys[0], n, mod)
 
+def bsgs(g, a, p):
+    # To solve g^e mod p = a and find e
+    m = ceil(sqrt(p-1))
+    # Baby Step
+    lookup_table = {pow(g, i, p): i for i in range(m)}
+    # Giant Step Precomputation c = g^(-m) mod p
+    c = pow(g, m*(p-2), p)
+    # Giant Step
+    for j in range(m):
+        x = (a*pow(c, j, p)) % p
+        if x in lookup_table:
+            return j*m + lookup_table[x]
+    return None
+
+def part1b():
+    #discrete logarithm problem.  solve using the baby step giant step algorithm.
+    #https://masterpessimistaa.wordpress.com/2018/01/14/dlp-and-baby-step-giant-step-algorithm/
+    mod = 20201227
+    n = bsgs(7, public_keys[0], mod)
+    return pow(public_keys[1], n, mod)
+
 print("Part 1: %d" % part1())
-print("Part 1: %d" % part1a())
+print("Part 1a: %d" % part1a())
+print("Part 1b: %d" % part1b())
