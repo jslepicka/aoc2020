f19318138e3ec417a2296e564ec0d1f2157ee20f
James Slepicka <james@slepicka.net>
2020-12-25 12:29:52 -0600

    day 25

---
+++ 25.py
@@ -0,0 +1,21 @@
+public_keys = []
+
+with open("25.txt") as f:
+    public_keys = [int(x.strip()) for x in f.readlines()]
+
+def part1():
+    #https://en.wikipedia.org/wiki/Modular_exponentiation
+    #We only need one loop value, so it really doesn't matter which public key
+    #we use for this first step.  The second one completes faster, so that's
+    #the one we'll use.
+    mod = 20201227
+    n = 1
+    while True:
+        if pow(7, n, mod) == public_keys[1]:            
+            break
+        n += 1
+    
+    #now use the loop val with the other public key to get the encryption key
+    return pow(public_keys[0], n, mod)
+
+print("Part 1: %d" % part1())
